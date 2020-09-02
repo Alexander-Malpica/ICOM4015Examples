d633fbbeda1ae9f17fa005dc81bf87eb9f971544
XenDark101 <alexander.malpica@upr.edu>
2020-09-02 17:43:58 -0400

    format

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 double Triangle::getArea() {
     double s = (this->sides[0] + this->sides[1] + this->sides[2]) / 2.0;
     double area = sqrt(s * (s - this->sides[0]) * (s - this->sides[1]) * (this->sides[2])); //Formula de Heros
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-// static double getArea(double s1, double s2, double s3) {
-//     double s = (s1 + s2 + s3) / 2.0;
-//     double area = sqrt(s * (s - s1) * (s - s2) * (s - s3)); //Formula de Heros
-//     return area;
-// }
-
 // static bool largerThan(Triangle t1, Triangle t2) 
 // {
 //     // Determine if first triangle is larger than the second
