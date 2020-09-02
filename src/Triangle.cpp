f8cea22e8fffa1747cf322ed026db715f7995949
XenDark101 <alexander.malpica@upr.edu>
2020-09-02 17:26:52 -0400

    Change individual double side for array of double

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 double Triangle::getArea() {
-    double s = (this->side1 + this->side2 + this->side3) / 2.0;
-    double area = sqrt(s * (s - this->side1) * (s - this->side2) * (this->side3)); //Formula de Heros
+    double s = (this->sides[0] + this->sides[1] + this->sides[2]) / 2.0;
+    double area = sqrt(s * (s - this->sides[0]) * (s - this->sides[1]) * (this->sides[2])); //Formula de Heros
     return area;
 }
